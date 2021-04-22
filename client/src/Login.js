e2fbd2f209ddfa9445aadf0bd8304a19fa569574
mansmahamat <mansour.mahamat.salle@gmail.com>
2021-04-22 18:04:08 +0200

    :art: fix url
    fix url

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Login.css'
 
-const AUTH_URL = 'https://accounts.spotify.com/authorize?client_id=39c957640b2940bc9b1ca9322bf20062&response_type=code&redirect_uri=https://spotify-clone-mans.netlify.app&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state'
+const AUTH_URL = 'https://accounts.spotify.com/authorize?client_id=39c957640b2940bc9b1ca9322bf20062&response_type=code&redirect_uri=https://spotify-clone-mans.netlify.app/&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state'
 
 export default function Login() {
     return (
